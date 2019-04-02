349a304db2790b02c222d59d25c5be3d4b3e711f
kmietotwy <kmietotwy@gmail.com>
2019-04-01 17:00:23 -1000

    secretNumber function

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,14 @@
 'use strict';
 module.exports = function() {
-  
+  let randomNumber = 0;
+
+  function SecretNumber(){
+    if(randomNumber === 0){
+      randomNumber = Math.random()*(1000000);
+    return randomNumber;
+    } else {
+      return randomNumber;
+    }
+  }
+  return SecretNumber;
 };
